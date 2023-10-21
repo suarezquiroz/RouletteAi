08f8b0f30ef262fa26d2031a4e40f74112daaf09
Santiago Suarez <santiago.suarez@talosdigital.com>
2023-10-21 18:28:48 -0500

    refactor and new data

--- nextzone.js
+++ nextzone.js
@@ -1,21 +1,12 @@
 import { readFileSync } from 'fs';
+import { getMinizone, getSide, roulette, roulettePosition } from './roulette.js';
 const fileData = readFileSync('./data/platinum.txt', 'utf-8');
 const data = fileData.split(/\r?\n/).map((d) => {
   return parseInt(d);
 });
 
 console.log('data points:', data.length);
-const roulette = [
-  0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5, 22, 34, 15, 3, 24, 36, 13, 1, -1, 27, 10, 25, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14,
-  2,
-];
 
-const roulettePosition = (n) => {
-  return roulette.indexOf(n);
-};
-
-const getMinizone = (p) => Math.floor(p / 2);
-const getSide = (p) => Math.floor(p / 19);
 const printList = (numberList, label) => {
   for (const number in numberList) {
     if (Object.hasOwnProperty.call(numberList, number)) {
@@ -41,11 +32,10 @@ const printList = (numberList, label) => {
           .map((n) => getSide(roulettePosition(n)))
           .reduce(
             (result, side) => {
-              if (side) result['left'] = result['left'] + 1;
-              else result['right'] = result['right'] + 1;
+              result[side] = result[side] + 1;
               return result;
             },
-            { right: 0, left: 0 }
+            { 0: 0, 1: 0, 2: 0, 3: 0 }
           )
       );
       console.log('hits:', list.length, 'unique:', filteredList.length, '\n');
@@ -84,7 +74,7 @@ data.forEach((n, index, arr) => {
 });
 
 const temperatures = frequencyData.map((hits, i) => [roulette[i], hits]).sort((a, b) => b[1] - a[1]);
-const hotNumbers = temperatures.slice(0, 12);
+const hotNumbers = temperatures.slice(0, 18);
 
 printList(nextNumbers, 'next: ');
 
@@ -110,9 +100,8 @@ const printNumbers = hotNumbers.map((h) => [
 ]);
 // console.log('Hot ones:\n\r');
 console.table(
-  hotNumbers
-    .map((h) => ({ number: h[0], hits: h[1], minizone: getMinizone(roulettePosition(h[0])) }))
-    .sort((a, b) => a.minizone - b.minizone)
+  hotNumbers.map((h) => ({ number: h[0], hits: h[1], minizone: getMinizone(roulettePosition(h[0])) }))
+  //.sort((a, b) => a.minizone - b.minizone)
 );
 
-console.table(nextNumbers);
+//console.table(nextNumbers);
---
+++ roulette.js
@@ -0,0 +1,20 @@
+export const getDozen = (n) => {
+  return Math.ceil(n / 12);
+};
+export const getcolumn = (n) => {
+  return n < 1 ? 0 : ((n + 2) % 3) + 1;
+};
+
+export const roulettePosition = (n) => {
+  return roulette.indexOf(n);
+};
+
+export const roulette = [
+  0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5, 22, 34, 15, 3, 24, 36, 13, 1, -1, 27, 10, 25, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14,
+  2,
+];
+
+export const getMinizone = (p) => Math.floor(p / 3);
+export const getSide = (p) => Math.floor(p / 9.501);
+
+export default { roulettePosition, getDozen, getcolumn, roulette, getMinizone, getSide };
